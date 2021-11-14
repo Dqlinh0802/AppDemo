92d897404a839c348ee1b497d171b524c2b145dd
Dqlinh0802 <dqlinh0802@gmail.com>
2021-11-14 19:17:43 +0700

    Icon in buttom Tab

--- AppMobile/components/main.js
+++ AppMobile/components/main.js
@@ -5,17 +5,6 @@ import {
     SafeAreaView,
     TouchableOpacity
 } from 'react-native'
-// import IntroComponent from './intro'
-// import Screen1 from './logo'
-// import Page1 from './Page1'
-// import Page2 from './Page2'
-// import Page3 from './Page3'
-// import History from './History'
-// import Messages from './Messages'
-// import HomePage from './HomePage'
-// import Account from './Account'
-// import vnexpress1 from './vnexpress1'
-// import vnexpress2 from './vnexpress2'
 import {
     IntroComponent, 
     Screen1, 
@@ -31,8 +20,9 @@ import {
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-vector-icons/icon'
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 const Stack = createNativeStackNavigator();
@@ -41,10 +31,26 @@ const Tab = createBottomTabNavigator();
 const Tabs = () =>{
   return(
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="HomePage" component={HomePage}/>
-            <Tab.Screen name="History" component={History} />    
-            <Tab.Screen name="Messages" component={Messages} />    
-            <Tab.Screen name="Account" component={Account} />   
+            <Tab.Screen name="HomePage" component={HomePage} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="home" size={27} color="black" />
+                )
+            }}/>
+            <Tab.Screen name="History" component={History} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="history" size={27} color="black" />
+                )
+            }}/>    
+            <Tab.Screen name="Messages" component={Messages} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <AntDesign name="message1" size={20} color="black" />
+                )
+            }}/>    
+            <Tab.Screen name="Account" component={Account} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="user" size={25} color="black" />
+                )
+            }}/>   
         </Tab.Navigator>
   );
 }
